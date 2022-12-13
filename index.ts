8067819b70ed2978db177f323933dc01d94d4fa3
Thales Minoru Sato <thalesms2@outlook.com>
2022-12-13 08:33:24 -0300

    tst headers

--- index.ts
+++ index.ts
@@ -23,10 +23,6 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE",);
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
     next();
 });
 
