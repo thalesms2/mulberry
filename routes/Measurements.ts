46db68c5fb1512b86276857dd84b532efb1eaa93
Thales Minoru Sato <thalesms2@outlook.com>
2022-12-14 08:13:51 -0300

    Revert "measurement"
    This reverts commit 79706013d711dbc583e7fb2421faddf603c85cd6.

--- routes/Measurements.ts
+++ routes/Measurements.ts
@@ -29,7 +29,6 @@ router.post("/", async (req, res) => {
                 initials: String(initials),
             },
         });
-        console.log(measurements)
         const result = {
             measurements: measurements,
             log: await generateLog(
@@ -43,9 +42,7 @@ router.post("/", async (req, res) => {
         res.sendStatus(204)
     }
 });
-5502093512739381
-1228
-933
+
 router.put("/", async (req, res) => {
     const { id, description, initials } = req.body;
     const result = await prisma.measurements.update({
