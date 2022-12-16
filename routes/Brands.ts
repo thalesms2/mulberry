a56e99c8ab75e4a7bed3e44547af3453f748e572
Thales Minoru Sato <thalesms2@outlook.com>
2022-12-16 12:27:01 -0300

    feat: brand route finished

--- routes/Brands.ts
+++ routes/Brands.ts
@@ -6,87 +6,109 @@ const router = express.Router();
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
-    const brands = await prisma.brands.findMany();
-    res.json(brands);
+    try {
+        const brands = await prisma.brands.findMany();
+        res.json(brands);
+    } catch (err) {
+        res.json(err)
+    }
 });
 
 router.get("/:id", async(req, res) => {
     const id = req.params.id
-    if(id) {
+    try{
         const brand = await prisma.brands.findUnique({
             where: { id: Number(id) },
         })
         res.json(brand)
+    } catch (err) {
+        res.json(err)
     }
 })
 
 router.post("/", async (req, res) => {
     const { description, userId } = req.body;
-    if (description && userId) {
-        const brand = await prisma.brands.create({
-            data: { description: String(description) },
-        });
-        const result = {
-            brand: brand,
-            log: await generateLog(
-                "CREATE",
-                `BRAND ${brand.id} - ${brand.description} CREATED`,
-                Number(userId)
-            ),
-        };
-        res.json(result);
-    } else {
-        res.sendStatus(204);
+    try {
+        if (description && userId) {
+            const brand = await prisma.brands.create({
+                data: { description: String(description) },
+            });
+            const result = {
+                brand: brand,
+                log: await generateLog(
+                    "CREATE",
+                    `BRAND ${brand.id} - ${brand.description} CREATED`,
+                    Number(userId)
+                ),
+            };
+            res.json(result);
+        } else {
+            res.sendStatus(400)
+        }
+    } catch (err) {
+        res.json(err)
     }
 });
 
 router.put("/", async (req, res) => {
     const { id, description } = req.body;
-    if (id && description) {
-        const oldBrand = await prisma.brands.findUnique({
-            where: { id: Number(id) },
-        });
-        const brand = await prisma.brands.update({
-            where: { id: Number(id) },
-            data: { description: String(description) },
-        });
-        const result = {
-            brand: brand,
-            log: await generateLog(
-                "EDIT",
-                `BRAND ${brand.id} EDITED - DESCRIPTION ${oldBrand.description} NEW DESCRIPTION ${brand.description} `,
-                Number(req.cookies.userId)
-            ),
-        };
-        res.json(result);
-    } else {
-        res.sendStatus(404);
+    try {
+        if (id && description) {
+            const oldBrand = await prisma.brands.findUnique({
+                where: { id: Number(id) },
+            });
+            const brand = await prisma.brands.update({
+                where: { id: Number(id) },
+                data: { description: String(description) },
+            });
+            const result = {
+                brand: brand,
+                log: await generateLog(
+                    "EDIT",
+                    `BRAND ${brand.id} EDITED - DESCRIPTION ${oldBrand.description} NEW DESCRIPTION ${brand.description} `,
+                    Number(req.cookies.userId)
+                ),
+            };
+            res.json(result);
+        } else {
+            res.sendStatus(400);
+        }
+    } catch (err) {
+        res.json(err)
     }
 });
 
 router.delete("/", async (req, res) => {
     const { id } = req.body;
-    if (id) {
-        const brand = await prisma.brands.delete({
-            where: { id: Number(id) },
-        });
-        const result = {
-            brand: brand,
-            log: await generateLog(
-                "DELETE",
-                `BRAND ${brand.id} - ${brand.description} DELETED`,
-                Number(req.cookies.userId)
-            ),
-        };
-        res.json(result);
-    } else {
-        res.sendStatus(404);
+    try {
+        if (id) {
+            const brand = await prisma.brands.delete({
+                where: { id: Number(id) },
+            });
+            const result = {
+                brand: brand,
+                log: await generateLog(
+                    "DELETE",
+                    `BRAND ${brand.id} - ${brand.description} DELETED`,
+                    Number(req.cookies.userId)
+                ),
+            };
+            res.json(result);
+        } else {
+            res.sendStatus(400);
+        }
+    } catch (err) {
+        res.json(err)
     }
 });
 
 router.delete("/all", async (req, res) => {
-    const result = await prisma.brands.deleteMany({});
-    res.json(result);
+    try {
+        const result = await prisma.brands.deleteMany({});
+        res.json(result);
+    } catch (err) {
+        res.json(err)
+    }
 });
 
 export default router;
